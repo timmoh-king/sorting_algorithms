846cb5ef82d47147660dd540ade398afea1dad8e
timmoh-king <timothykariuki43@gmail.com>
2023-04-26 10:56:47 -0700

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,6 +12,9 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j, flag = 0;
 	int temp;
 
+	if (array == NULL || size < 2)
+		return;
+
 	for (i = 0; i < (size - 1); i++)
 	{
 		flag = 0;
@@ -23,6 +26,7 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				flag = 1;
+				print_array(array, size);
 			}
 		}
 		if (flag == 0)
