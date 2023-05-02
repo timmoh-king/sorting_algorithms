32f3266a936ba14ba509db698b5d9cc9b4b4fb4b
timmoh-king <timothykariuki43@gmail.com>
2023-05-02 11:26:25 -0700

    void quick_sort(int *array, size_t size)

---
+++ 2-selection_sort.c
@@ -0,0 +1,10 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ * @array: the array
+ * @size: the size of the array
+ *
+ * Return: nothing
+ */
+void selection_sort(int *array, size_t size)
---
+++ 3-quick_sort.c
@@ -0,0 +1,10 @@
+#include "sort.h"
+
+/**
+ * quick_sort - sorts an array of integers in ascending order
+ * @array: the array
+ * @size: the sze of the array
+ *
+ * Return: nothing
+ */
+void quick_sort(int *array, size_t size)
